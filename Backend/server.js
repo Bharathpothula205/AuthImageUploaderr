002c1f1bcfd57fab443910f75e5e4468bb1347be
Bharath P <bharathkkl907@gmail.com>
2025-03-28 10:38:40 +0530

    All the other stuff for packages

--- Backend/server.js
+++ Backend/server.js
@@ -1,18 +1,20 @@
 const express=require('express');
 const cors=require("cors")
 require('dotenv').config();
-
+const connectDB =require('./Configs/ConnectDb');
+const authRoutes=require('./Routes/authRoutes')
+const postRoutes=require('./Routes/postRoute')
 
 const app=express();
-
+connectDB();
 
 //Middlewares
 app.use(express.json());
 app.use(cors());
 
 //Routes
-
-
+app.use('/api/auth',authRoutes)
+app.use('/api/posts',postRoutes)
 
 const PORT=process.env.PORT || 5000;
 app.listen(PORT,()=>console.log(`Server is Online at PORT ${PORT}`));
